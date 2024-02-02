a171f3937a67b7ba1b72f892d1bd1ced4e044377
mrana007 <rana.es.bc@gmail.com>
2024-02-02 12:50:27 +0600

    added portfolio live link

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -43,7 +43,8 @@ const ContactMe = () => {
             <form
               className="flex flex-col items-center"
               ref={form}
-              onSubmit={sendEmail}>
+              onSubmit={sendEmail}
+            >
               <div className="md:w-3/4 lg:w-2/3 xl:w-1/2">
                 <div className="flex flex-col md:flex-row">
                   <input
@@ -52,14 +53,16 @@ const ContactMe = () => {
                     type="text"
                     className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full md:w-1/2 md:mr-2 outline-none focus:ring-2 focus:ring-blue-600"
                     placeholder="Name"
-                    required/>
+                    required
+                  />
                   <input
                     id="email"
                     name="to_email"
                     type="email"
                     className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full md:w-1/2 md:ml-2 outline-none focus:ring-2 focus:ring-blue-600"
                     placeholder="Email"
-                    required/>
+                    required
+                  />
                 </div>
                 {/* <input
             id="subject"
@@ -75,12 +78,14 @@ const ContactMe = () => {
                   placeholder="Say Something"
                   className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full outline-none focus:ring-2 focus:ring-blue-600"
                   defaultValue={""}
-                  required/>
+                  required
+                />
               </div>
               <button
                 type="submit"
                 value="Send"
-                className="border-2 text-md mt-5 rounded-md border-none py-2 px-4 bg-green-600 hover:bg-blue-700 text-gray-100 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600">
+                className="border-2 text-md mt-5 rounded-md border-none py-2 px-4 bg-green-600 hover:bg-blue-700 text-gray-100 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600"
+              >
                 Send Message
               </button>
             </form>
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -15,7 +15,6 @@ const Banner = () => {
       position: "relative",
     }}>
       <div className="absolute inset-0 bg-[#131313] bg-opacity-60"></div>
-
       <div className="text-white min-h-screen pt-4 pl-8 md:pl-52 flex items-center relative">
         <div className="w-full">
           <h1 className="text-left pb:3 md:pb-5 text-4xl md:text-8xl">
